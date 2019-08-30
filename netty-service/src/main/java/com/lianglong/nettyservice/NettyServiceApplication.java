9800819e93e09fa505ec3225a86cc193e5433630
nevermore <nevermore_sf@aliyun.com>
2019-08-30 17:10:33 +0800

    socket

---
+++ netty-service/src/main/java/com/lianglong/nettyservice/NettyServiceApplication.java
@@ -0,0 +1,13 @@
+package com.lianglong.nettyservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NettyServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NettyServiceApplication.class, args);
+    }
+
+}
